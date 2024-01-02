352283d0ad761adb7f4497f4abbfce3e1f168039
Farhan Shakil <farhan.shakil@koderlabs.com>
2024-01-02 21:26:50 +0500

    security hotspot removed

--- android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.frontend
 
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 class MainActivity: FlutterFragmentActivity() {
 }
